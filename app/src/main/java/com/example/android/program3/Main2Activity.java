dbf1bb26abaa9240d048516afae93a2655618473
Kenisha Jones <kjones47@binghamton.edu>
2019-10-01 23:00:32 -0400

    surface class

--- app/src/main/java/com/example/android/program3/Main2Activity.java
+++ app/src/main/java/com/example/android/program3/Main2Activity.java
@@ -39,5 +39,4 @@ public class Main2Activity extends AppCompatActivity {
             }
         }
     }
-
 }
---
+++ app/src/main/java/com/example/android/program3/Surface.java
@@ -0,0 +1,16 @@
+package com.example.android.program3;
+
+import android.view.SurfaceView;
+
+public class Surface extends SurfaceView {
+
+    public Surface(){
+        super();
+
+    }
+    public drawBrick() {
+    }
+    public drawBall(){
+
+    }
+}
