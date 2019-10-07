5364fcad0aad6481da71ae20dfb9103fbce7134d
Kenisha Jones <kjones47@binghamton.edu>
2019-10-06 23:55:52 -0400

    add brick and ball

--- app/src/main/java/com/example/android/program3/Surface.java
+++ app/src/main/java/com/example/android/program3/Surface.java
@@ -1,16 +1,68 @@
 package com.example.android.program3;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.SurfaceView;
+import android.view.SurfaceHolder;
 
-public class Surface extends SurfaceView {
+public class Surface extends SurfaceView implements SurfaceHolder.Callback {
 
-    public Surface(){
-        super();
+    private SurfaceHolder holder = null;
+    private Paint paint = null;
+    private float x, y = 0;
+    private String title;
+    private String xAxis;
+    private String yAxis;
+    private int minX;
+    private int maxX;
+    private int minY;
+    private int maxY;
+
+    public Surface(Context context){
+        super(context);
+        holder = getHolder();
+        paint = new Paint();
+        paint.setColor(Color.RED);
 
     }
-    public drawBrick() {
-    }
-    public drawBall(){
 
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder){
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int left, int top, int i){
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder){
+        paint = null;
+    }
+
+    public void drawBrick() {
+        holder = getHolder();
+        Canvas canvas = holder.lockCanvas();
+
+
+        // Draw the rectangle.
+        canvas.drawRect(x, y, x + 200, y + 200, paint);
+
+        holder.unlockCanvasAndPost(canvas);
+
+    }
+    public void drawBall(){
+
+        holder = getHolder();
+        Canvas canvas = holder.lockCanvas();
+        for(int i=0;i<50;i++){
+            canvas.drawCircle(100, 100, 10, paint);
+        }
+
+
+        holder.unlockCanvasAndPost(canvas);
     }
 }
