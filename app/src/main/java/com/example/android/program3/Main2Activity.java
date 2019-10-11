7de66e2d742742f9c3236d82e753e370329f03cb
Kenisha Jones <kjones47@binghamton.edu>
2019-10-10 23:06:34 -0400

    level 1 bricks

--- app/src/main/java/com/example/android/program3/Main2Activity.java
+++ app/src/main/java/com/example/android/program3/Main2Activity.java
@@ -2,6 +2,7 @@ package com.example.android.program3;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,32 +11,120 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.MotionEvent;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnTouchListener {
+
+    private Button redButton = null;
+
+    private Button greenButton = null;
+
+    private boolean drawBall = true;
+
+    private LinearLayout canvasLayout = null;
+
+    Surface customSurfaceView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        setTitle("SurfaceView");
+
+        initControls();
+
+        // Hide the app title bar.
+       // getSupportActionBar().hide();
+
+        // Make app full screen to hide top status bar.
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Create custom surfaceview object.
+        customSurfaceView = new Surface(getApplicationContext());
+
+        // Set this as the onTouchListener to process custom surfaceview ontouch event.
+        customSurfaceView.setOnTouchListener(this);
+
+        // Add the custom surfaceview object to the layout.
+        canvasLayout.addView(customSurfaceView);
+
+        // Click this button to draw a red circle ball move after finger touch.
+        redButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                drawBall = true;
             }
         });
 
-      /*  Brick [][] levelOne = new Brick[10][6];
-        for(int i=0;i<levelOne.length;i++){
-            for(int j=0;j<levelOne[i].length;j++){
-                Brick b = levelOne[i][j];
-                b.setColor(Color.RED);
-                b.makeBrick();
+        // Click this button to draw a green rectangle move after finger touch.
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawBall = false;
             }
-        }*/
+        });
+
+    }
+
+    /* Initialise ui controls. */
+    private void initControls() {
+        if (redButton == null) {
+            redButton = (Button) findViewById(R.id.redButton);
+        }
+
+        if (greenButton == null) {
+            greenButton = (Button) findViewById(R.id.greenButton);
+        }
+
+        // This layout is used to contain custom surfaceview object.
+        if (canvasLayout == null) {
+            canvasLayout = (LinearLayout) findViewById(R.id.customViewLayout);
+        }
+    }
+
+    /* If user finger touch the surfaceview object. */
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        // If user touch the custom SurfaceView object.
+        if (view instanceof SurfaceView) {
+            view.invalidate();
+
+            float x = motionEvent.getX();
+
+            float y = motionEvent.getY();
+
+            customSurfaceView.setCircleX(x);
+
+            customSurfaceView.setCircleY(y);
+
+            if (drawBall) {
+                // Create and set a red paint to custom surfaceview.
+                Paint paint = new Paint();
+                paint.setColor(Color.RED);
+                customSurfaceView.setPaint(paint);
+
+                customSurfaceView.drawBricks();
+            } else {
+                // Create and set a green paint to custom surfaceview.
+                Paint paint = new Paint();
+                paint.setColor(Color.GREEN);
+                customSurfaceView.setPaint(paint);
+
+                customSurfaceView.drawRect();
+            }
+
+            // Tell android os the onTouch event has been processed.
+            return true;
+        } else {
+            // Tell android os the onTouch event has not been processed.
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/android/program3/Surface.java
+++ app/src/main/java/com/example/android/program3/Surface.java
@@ -8,127 +8,116 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class Surface extends SurfaceView implements SurfaceHolder.Callback {
-
-    private SurfaceHolder holder = null;
+    private SurfaceHolder surfaceHolder = null;
     private Paint paint = null;
-    private float x, y = 0;
-    private String title;
-    private String xAxis;
-    private String yAxis;
-    private int minX;
-    private int maxX;
-    private int minY;
-    private int maxY;
+    private float circleX = 0;
+    private float circleY = 0;
 
     public Surface(Context context) {
         super(context);
-        holder = getHolder();
+
+        surfaceHolder = getHolder();
+
         paint = new Paint();
         paint.setColor(Color.RED);
-        title = "Title";
-        xAxis = "X-axis";
-        yAxis = "Y-axis";
-        minX = 0;
-        minY = 0;
-        maxX = 10;
-        maxY = 100;
+
+        // this.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        // paint.setStyle(Style.FILL);
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder){
-        drawGraph(10,10);
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        drawBall();
     }
-
     @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int left, int top, int i){
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
 
     }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder){
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
         paint = null;
+
     }
 
-    public void drawPoint() {
-        holder = getHolder();
-
-        Canvas c = holder.lockCanvas();
-
-        paint.setColor(Color.BLUE);
-        c.drawCircle(x, y, 10, paint);
-
-        holder.unlockCanvasAndPost(c);
-    }
-
-    public void drawRect()
+    /* This method will be invoked to draw a circle in canvas. */
+    public void drawBall()
     {
-        holder = getHolder();
-        Canvas canvas = holder.lockCanvas();
+        surfaceHolder = getHolder();
 
-
-        // Draw the rectangle.
-        canvas.drawRect(x, y, x + 200, y + 200, paint);
-
-        holder.unlockCanvasAndPost(canvas);
-    }
-
-    public void drawDot(int c, float[]x, float[]y)
-    {
-        holder = getHolder();
-        Canvas canvas = holder.lockCanvas();
-        for(int i=0;i<c;i++){
-            canvas.drawCircle(x[i], y[i], 10, paint);
-        }
-
-
-        holder.unlockCanvasAndPost(canvas);
-    }
-
-    public void drawGraph(int x, int y)
-    {
-        Canvas canvas = holder.lockCanvas();
+        // Get and lock canvas object from surfaceHolder.
+        Canvas canvas = surfaceHolder.lockCanvas();
 
         Paint surfaceBackground = new Paint();
         // Set the surfaceview background color.
-        surfaceBackground.setColor(Color.WHITE);
+        surfaceBackground.setColor(Color.BLACK);
         // Draw the surfaceview background color.
         canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), surfaceBackground);
 
-        // Draw the graph
-        paint.setColor(Color.BLACK);
-        canvas.drawRect(80, 20, 100, this.getHeight() - 80, paint);
-        //paint.setColor(Color.RED);
-        canvas.drawRect(80, this.getHeight()-100, this.getWidth()-50, this.getHeight() - 80 , paint);
+        // Draw the circle.
+        paint.setColor(Color.RED);
+        canvas.drawCircle(circleX, circleY, 100, paint);
 
-        //Draw 0, max X and Y
-        paint.setColor(Color.BLUE);
-        paint.setTextSize(50);
-        canvas.drawText("" + x,0,60,paint);
-        canvas.drawText("0",0,this.getHeight()-20,paint);
-        canvas.drawText("" + y, this.getWidth()-150, this.getHeight()-20, paint);
-        //Draw x and y axis
-        paint.setColor(Color.BLUE);
-        paint.setTextSize(50);
+        canvas.drawCircle(50, 50, 200, paint);
 
-        holder.unlockCanvasAndPost(canvas);
+        // Unlock the canvas object and post the new draw.
+        surfaceHolder.unlockCanvasAndPost(canvas);
     }
 
+    /* This method will be invoked to draw a circle in canvas. */
+    public void drawRect()
+    {
+        Canvas canvas = surfaceHolder.lockCanvas();
 
+        Paint surfaceBackground = new Paint();
+        // Set the surfaceview background color.
+        surfaceBackground.setColor(Color.BLUE);
+        // Draw the surfaceview background color.
+        canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), surfaceBackground);
+
+        // Draw the rectangle.
+        canvas.drawRect(circleX, circleY, circleX + 200, circleY + 200, paint);
+
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
+    public void drawBricks()
+    {
+        Canvas canvas = surfaceHolder.lockCanvas();
+
+        Paint surfaceBackground = new Paint();
+        // Set the surfaceview background color.
+        surfaceBackground.setColor(Color.BLUE);
+        // Draw the surfaceview background color.
+        canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), surfaceBackground);
+
+        // Draw the rectangle.
+        int height = 30;
+        int end = this.getHeight()-100;
+        for(int j = height; j <500; j=j+85) {
+            for (int i = 30; i < this.getWidth(); i = i + 160) {
+                canvas.drawRect(i, j, i + 150, j+ 75, paint);
+            }
+        }
+
+        canvas.drawRect(this.getWidth()/3, this.getHeight()-200, this.getWidth()/2 + 300, this.getHeight()-125, paint);
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
 
     public float getCircleX() {
-        return x;
+        return circleX;
     }
 
     public void setCircleX(float circleX) {
-        this.x = circleX;
+        this.circleX = circleX;
     }
 
-    public float getCircleY(){
-        return y;
+    public float getCircleY() {
+        return circleY;
     }
 
     public void setCircleY(float circleY) {
-        this.y = circleY;
+        this.circleY = circleY;
     }
 
     public Paint getPaint() {
@@ -138,5 +127,4 @@ public class Surface extends SurfaceView implements SurfaceHolder.Callback {
     public void setPaint(Paint paint) {
         this.paint = paint;
     }
-
 }
